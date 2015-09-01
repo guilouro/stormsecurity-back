438219ee213fe008fce00b8ba624715b5360eda4
Guilherme Louro <guilherme-louro@hotmail.com>
2015-09-01 13:52:44 -0300

    generic list view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 from django.conf.urls import patterns, url
-from core.views import MovieDetail
+from core.views import MovieDetail, HomeList
 
 urlpatterns = patterns(
     'core.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', HomeList.as_view(), name='home'),
     url(r'^filme/(?P<slug>[\w-]+)/$', MovieDetail.as_view(), name='movie_detail'),
 )
--- core/views.py
+++ core/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from core.models import Movie, Genre, Actor
 from django.views.generic import DetailView, ListView
 
-# Create your views here.
-def home(request):
-    if 'order_by' in request.GET:
-        movies = Movie.objects.all().order_by(request.GET['order_by'])
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'core/index.html', {'movies': movies})
+
+# def home(request):
+#     if 'order_by' in request.GET:
+#         movies = Movie.objects.all().order_by(request.GET['order_by'])
+#     else:
+#         movies = Movie.objects.all()
+#     return render(request, 'core/index.html', {'movies': movies})
+
+
+class HomeList(ListView):
+    model = Movie
+    template_name = 'core/index.html'
 
 
 class MovieDetail(DetailView):
