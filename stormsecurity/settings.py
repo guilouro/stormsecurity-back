ea313fa50bd69c43ee71486af131a089187a223e
Guilherme Louro <guilherme-louro@hotmail.com>
2015-08-31 22:57:58 -0300

    start project

---
+++ stormsecurity/settings.py
@@ -0,0 +1,103 @@
+"""
+Django settings for stormsecurity project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from unipath import Path
+from dj_database_url import parse
+
+BASE_DIR = Path(__file__).parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+&z@gs2%xc)wxpvkt&$o56^ojd+kwh@ylnmf)q35tm5z-!t3te'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Apps #
+    'core',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
+)
+
+ROOT_URLCONF = 'stormsecurity.urls'
+
+WSGI_APPLICATION = 'stormsecurity.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': parse('sqlite:///' + BASE_DIR.child('db.sqlite3'))
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'pt-BR'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Template path
+TEMPLATE_DIRS = (
+    BASE_DIR.parent.child('templates'),
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATICFILES_DIRS = (
+    BASE_DIR.parent.child('assets'),
+)
+
+STATIC_ROOT = BASE_DIR.parent.child('static')
+STATIC_URL = '/static/'
+
+
+####################################
+# load settings_local.py if exists
+####################################
+try:
+    execfile(BASE_DIR.child('settings_local.py'), globals(), locals())
+except IOError:
+    pass
