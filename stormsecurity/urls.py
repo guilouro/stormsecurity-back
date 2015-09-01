2def3a677e72c76e07c616f5f4b84dfff8b18f63
Guilherme Louro <guilherme-louro@hotmail.com>
2015-09-01 17:15:18 -0300

    page 404

--- stormsecurity/urls.py
+++ stormsecurity/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns(
     '',
@@ -15,3 +16,8 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT}))
+
+    urlpatterns += patterns('',
+        (r'^500/$', TemplateView.as_view(template_name="404.html")),
+        (r'^404/$', TemplateView.as_view(template_name="404.html")),
+    )
